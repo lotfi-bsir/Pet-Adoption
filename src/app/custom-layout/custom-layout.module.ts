2224c1549fcfdc9a7df060fe3022c2c73bcd1831
lotfi-bsir <lotfi.bsir@esprit.tn>
2023-02-17 19:24:49 +0100

    create project

---
+++ src/app/custom-layout/custom-layout.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutModule } from '../../@vex/layout/layout.module';
+import { CustomLayoutComponent } from './custom-layout.component';
+import { SidenavModule } from '../../@vex/layout/sidenav/sidenav.module';
+import { ToolbarModule } from '../../@vex/layout/toolbar/toolbar.module';
+import { FooterModule } from '../../@vex/layout/footer/footer.module';
+import { ConfigPanelModule } from '../../@vex/components/config-panel/config-panel.module';
+import { SidebarModule } from '../../@vex/components/sidebar/sidebar.module';
+import { QuickpanelModule } from '../../@vex/layout/quickpanel/quickpanel.module';
+import { HomeComponent } from './home/home.component';
+import {RouterModule} from "@angular/router";
+import { HeaderComponent } from './home/header/header.component';
+import { MainComponent } from './home/main/main.component';
+import { FooterComponent } from './home/footer/footer.component';
+
+
+@NgModule({
+  declarations: [CustomLayoutComponent, HomeComponent, HeaderComponent, MainComponent, FooterComponent],
+  imports: [
+    CommonModule,
+    LayoutModule,
+    SidenavModule,
+    ToolbarModule,
+    FooterModule,
+    ConfigPanelModule,
+    SidebarModule,
+    QuickpanelModule,
+    RouterModule
+  ]
+})
+export class CustomLayoutModule {
+}
